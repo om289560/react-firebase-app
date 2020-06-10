47dbb0800b9aa32adc8857f048f91876b642af49
Oscar Miranda <omiranda0058@gmail.com>
2020-06-09 19:04:11 -0500

    added property for register user in app.js along with changes for render

--- App.js
+++ App.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { f, auth, database } from './config/config.js';
+
+export default class App extends React.Component {
+
+  registerUser = (email, password) => {
+
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>This is Instagram</Text>
+      </View>
+    );
+  }
+}
 //code from template.
 // export default function App() {
 //   return (
